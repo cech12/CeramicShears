5f303b65c7b925c28acd5695281cb1ce780fca51
Christian Voß <cech12@gmail.com>
2021-06-21 19:21:42 +0200

    add some world interaction tests

--- src/test/java/cech12/ceramicshears/IntegrationTestUtils.java
+++ src/test/java/cech12/ceramicshears/IntegrationTestUtils.java
@@ -5,14 +5,21 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.PacketDirection;
+import net.minecraft.network.play.ServerPlayNetHandler;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraftforge.common.util.FakePlayerFactory;
 
 import java.util.ArrayList;
@@ -48,20 +55,79 @@ public class IntegrationTestUtils {
     }
 
     public static ActionResult<ItemStack> useItem(IntegrationTestHelper helper, BlockPos pos, ItemStack stack) {
+        return useItem(helper, pos, Direction.UP, stack);
+    }
+
+    private static void placePlayer(PlayerEntity player, BlockPos pos, Direction direction) {
+        double x = pos.getX() + 0.5D;
+        double y = pos.getY();
+        double z = pos.getZ() + 0.5D;
+        float xRot = 0F;
+        float yRot = 0F;
+        switch (direction) {
+            case UP:
+                y += 1.0D;
+                yRot = 90F;
+                break;
+            case DOWN:
+                y -= 2.0D;
+                yRot = -90F;
+                break;
+            case NORTH:
+                y -= 1.0D;
+                z += 1.0D;
+                xRot = 0F;
+                break;
+            case EAST:
+                y -= 1.0D;
+                x -= 1.0D;
+                xRot = 90F;
+                break;
+            case SOUTH:
+                y -= 1.0D;
+                z -= 1.0D;
+                xRot = -180F;
+                break;
+            case WEST:
+                y -= 1.0D;
+                x += 1.0D;
+                xRot = -90F;
+                break;
+        }
+        player.absMoveTo(x, y, z, xRot, yRot);
+    }
+
+    public static ActionResult<ItemStack> useItem(IntegrationTestHelper helper, BlockPos pos, Direction direction, ItemStack stack) {
         return helper.relativePos(pos).map(actualPos -> {
-            PlayerEntity player = FakePlayerFactory.getMinecraft(helper.getWorld()); // This is required because forge NPEs in place block
+            ServerPlayerEntity player = FakePlayerFactory.getMinecraft(helper.getWorld());
             player.setItemInHand(Hand.MAIN_HAND, stack);
-            //centered one block above looking down
-            player.absMoveTo(actualPos.getX() + 0.5D, actualPos.getY() + 1.0D, actualPos.getZ() + 0.5D, 0F, 90F);
-            return stack.use(helper.getWorld(), player, Hand.MAIN_HAND);
+            placePlayer(player, actualPos, direction);
+            BlockRayTraceResult rayTrace = new BlockRayTraceResult(Vector3d.ZERO, direction, actualPos, false);
+            ActionResultType actionResultType = player.gameMode.useItemOn(player, helper.getWorld(), stack, Hand.MAIN_HAND, rayTrace);
+            return new ActionResult<>(actionResultType, player.getItemInHand(Hand.MAIN_HAND));
         }).orElse(ActionResult.fail(ItemStack.EMPTY));
     }
 
     public static ActionResult<ItemStack> useItemOnEntity(IntegrationTestHelper helper, Entity entity, ItemStack stack) {
-        PlayerEntity player = FakePlayerFactory.getMinecraft(helper.getWorld()); // This is required because forge NPEs in place block
+        PlayerEntity player = FakePlayerFactory.getMinecraft(helper.getWorld());
         player.setItemInHand(Hand.MAIN_HAND, stack);
         ActionResultType actionResultType = player.interactOn(entity, Hand.MAIN_HAND);
         return new ActionResult<>(actionResultType, player.getItemInHand(Hand.MAIN_HAND));
+    }
+
+    public static ActionResult<ItemStack> destroyBlockWithItem(IntegrationTestHelper helper, BlockPos pos, ItemStack stack) {
+        return helper.relativePos(pos).map(actualPos -> {
+            ServerPlayerEntity player = FakePlayerFactory.getMinecraft(helper.getWorld());
+            player.setItemInHand(Hand.MAIN_HAND, stack);
+            placePlayer(player, actualPos, Direction.DOWN);
+            ActionResultType actionResultType = ActionResultType.FAIL;
+            //fake player of forge needs a dummy connection here to "send" world updates
+            player.connection = new ServerPlayNetHandler(helper.getWorld().getServer(), new NetworkManager(PacketDirection.SERVERBOUND), player);
+            if (player.gameMode.destroyBlock(actualPos)) {
+                actionResultType = ActionResultType.SUCCESS;
+            }
+            return new ActionResult<>(actionResultType, player.getItemInHand(Hand.MAIN_HAND));
+        }).orElse(ActionResult.fail(ItemStack.EMPTY));
     }
 
 }
---
+++ src/test/java/cech12/ceramicshears/integration/WorldTests.java
@@ -0,0 +1,100 @@
+package cech12.ceramicshears.integration;
+
+import cech12.ceramicshears.IntegrationTestUtils;
+import cech12.ceramicshears.api.item.CeramicShearsItems;
+import com.alcatrazescapee.mcjunitlib.framework.IntegrationTest;
+import com.alcatrazescapee.mcjunitlib.framework.IntegrationTestClass;
+import com.alcatrazescapee.mcjunitlib.framework.IntegrationTestHelper;
+import net.minecraft.block.BeehiveBlock;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.RedstoneTorchBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+
+@IntegrationTestClass(value = "world")
+public class WorldTests {
+
+    private static final BlockPos INTERACTION_POSITION = new BlockPos(1, 1, 1);
+
+    @IntegrationTest(value = "pumpkin")
+    public void testInteractionWithPumpkin(IntegrationTestHelper helper) {
+        ItemStack shears = new ItemStack(CeramicShearsItems.CERAMIC_SHEARS);
+
+        ActionResult<ItemStack> result = IntegrationTestUtils.useItem(helper, INTERACTION_POSITION, Direction.NORTH, shears);
+
+        helper.assertTrue(() -> result.getResult() == ActionResultType.SUCCESS || result.getResult() == ActionResultType.CONSUME, "Block interaction should be marked as successful or consumed");
+        helper.assertTrue(() -> result.getObject().getDamageValue() == 1, "Shears should loose one durability point");
+        helper.assertBlockAt(INTERACTION_POSITION, Blocks.CARVED_PUMPKIN, "Pumpkin should be transformed to a carved pumpkin");
+    }
+
+    private void testBeehiveBlocksForHoneyLevel(IntegrationTestHelper helper, int honeyLevel) {
+        helper.setBlockState(INTERACTION_POSITION, helper.getBlockState(INTERACTION_POSITION).setValue(BeehiveBlock.HONEY_LEVEL, honeyLevel));
+        helper.setBlockState(INTERACTION_POSITION.above(), helper.getBlockState(INTERACTION_POSITION).setValue(BeehiveBlock.HONEY_LEVEL, honeyLevel));
+
+        ActionResult<ItemStack> beeNestResult = IntegrationTestUtils.useItem(helper, INTERACTION_POSITION, Direction.NORTH, new ItemStack(CeramicShearsItems.CERAMIC_SHEARS));
+        ActionResult<ItemStack> beeHiveResult = IntegrationTestUtils.useItem(helper, INTERACTION_POSITION.above(), Direction.NORTH, new ItemStack(CeramicShearsItems.CERAMIC_SHEARS));
+
+        if (honeyLevel < 5) {
+            helper.assertTrue(() -> beeNestResult.getResult() == ActionResultType.PASS, "Block interaction with bee nest at honey level " + honeyLevel + " should be marked as passed");
+            helper.assertTrue(() -> beeHiveResult.getResult() == ActionResultType.PASS, "Block interaction with beehive at honey level " + honeyLevel + " should be marked as passed");
+            helper.assertTrue(() -> beeNestResult.getObject().getDamageValue() == 0, "Shears should not loose a durability point when interacting with bee nest at honey level " + honeyLevel);
+            helper.assertTrue(() -> beeHiveResult.getObject().getDamageValue() == 0, "Shears should not loose a durability point when interacting with beehive at honey level " + honeyLevel);
+            helper.assertBlockAt(INTERACTION_POSITION, blockState -> blockState.getValue(BeehiveBlock.HONEY_LEVEL) == honeyLevel, "Honey level " + honeyLevel + " of bee nest should remain when interacting with shears");
+            helper.assertBlockAt(INTERACTION_POSITION.above(), blockState -> blockState.getValue(BeehiveBlock.HONEY_LEVEL) == honeyLevel, "Honey level " + honeyLevel + " of beehive should remain when interacting with shears");
+        } else {
+            helper.assertTrue(() -> beeNestResult.getResult() == ActionResultType.SUCCESS || beeNestResult.getResult() == ActionResultType.CONSUME, "Block interaction with bee nest at honey level " + honeyLevel + " should be marked as successful or consumed");
+            helper.assertTrue(() -> beeHiveResult.getResult() == ActionResultType.SUCCESS || beeNestResult.getResult() == ActionResultType.CONSUME, "Block interaction with beehive at honey level " + honeyLevel + " should be marked as successful or consumed");
+            helper.assertTrue(() -> beeNestResult.getObject().getDamageValue() == 1, "Shears should loose a durability point when interacting with bee nest at honey level " + honeyLevel);
+            helper.assertTrue(() -> beeHiveResult.getObject().getDamageValue() == 1, "Shears should loose a durability point when interacting with beehive at honey level " + honeyLevel);
+            helper.assertBlockAt(INTERACTION_POSITION, blockState -> blockState.getValue(BeehiveBlock.HONEY_LEVEL) == 0, "Honey level " + honeyLevel + " of bee nest should be set to 0 when interacting with shears");
+            helper.assertBlockAt(INTERACTION_POSITION.above(), blockState -> blockState.getValue(BeehiveBlock.HONEY_LEVEL) == 0, "Honey level " + honeyLevel + " of beehive should be set to 0 when interacting with shears");
+        }
+    }
+
+    @IntegrationTest(value = "beehives")
+    public void testInteractionWithBeehiveLevel0(IntegrationTestHelper helper) {
+        testBeehiveBlocksForHoneyLevel(helper, 0);
+    }
+
+    @IntegrationTest(value = "beehives")
+    public void testInteractionWithBeehiveLevel1(IntegrationTestHelper helper) {
+        testBeehiveBlocksForHoneyLevel(helper, 1);
+    }
+
+    @IntegrationTest(value = "beehives")
+    public void testInteractionWithBeehiveLevel2(IntegrationTestHelper helper) {
+        testBeehiveBlocksForHoneyLevel(helper, 2);
+    }
+
+    @IntegrationTest(value = "beehives")
+    public void testInteractionWithBeehiveLevel3(IntegrationTestHelper helper) {
+        testBeehiveBlocksForHoneyLevel(helper, 3);
+    }
+
+    @IntegrationTest(value = "beehives")
+    public void testInteractionWithBeehiveLevel4(IntegrationTestHelper helper) {
+        testBeehiveBlocksForHoneyLevel(helper, 4);
+    }
+
+    @IntegrationTest(value = "beehives")
+    public void testInteractionWithBeehiveLevel5(IntegrationTestHelper helper) {
+        testBeehiveBlocksForHoneyLevel(helper, 5);
+    }
+
+    @IntegrationTest(value = "tripwire")
+    public void testBreakingTripwire(IntegrationTestHelper helper) {
+        final BlockPos tripwirePos = new BlockPos(3, 1, 1);
+        final BlockPos redstoneTorchPos = new BlockPos(0, 1, 0);
+
+        ActionResult<ItemStack> actionResult = IntegrationTestUtils.destroyBlockWithItem(helper, tripwirePos, new ItemStack(CeramicShearsItems.CERAMIC_SHEARS));
+
+        helper.assertAirAt(tripwirePos, "Tripwire should not be there after breaking it with shears");
+        helper.assertBlockAt(redstoneTorchPos, blockState -> !blockState.getValue(RedstoneTorchBlock.LIT), "Redstone torch should not be lit when tripwire is broken by shears");
+        helper.assertTrue(() -> actionResult.getResult() == ActionResultType.SUCCESS || actionResult.getResult() == ActionResultType.CONSUME, "Tripwire breaking should be marked as successful or consumed");
+        helper.assertTrue(() -> actionResult.getObject().getDamageValue() == 1, "Shears should loose a durability point when breaking tripwire");
+    }
+
+}
